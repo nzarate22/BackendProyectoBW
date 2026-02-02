5f09b9d59aa93ecf227e38afe10d06dcc4c2caf3
Natalia Zárate <nataliazarate213@gmail.com>
2026-02-01 20:08:32 -0600

    Add JsonBackReference annotation to Receta class

--- src/main/java/com/example/BackendProyectoBW/Entidades/Receta.java
+++ src/main/java/com/example/BackendProyectoBW/Entidades/Receta.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -16,6 +17,7 @@ public class Receta {
     private Integer idReceta;
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name = "idProducto")
+    @JsonBackReference
     private Producto producto;
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name = "idIngrediente")
